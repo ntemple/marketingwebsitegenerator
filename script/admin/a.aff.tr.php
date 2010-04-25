119f08e78f409bfb3433fb01ed969596190bc8e4
Nick Temple <ntemple@users.noreply.github.com>
2010-04-25 08:28:58 +0000

    admin reskin

--- script/admin/a.aff.tr.php
+++ script/admin/a.aff.tr.php
@@ -14,11 +14,11 @@
  */
 
  
-session_start();
 	include("inc.top.php");
 	$q=new Cdb;
 	$q3=new Cdb;
 	$cmember="";
+  $script = '';
 	FFileRead("templates/a.admin.template.aff.tr.htm",$cmember);
 	$sta=$status;
 	FFileRead("templates/a.admin.template.aff.tr.row.htm",$row);
@@ -207,32 +207,47 @@ session_start();
 	$cmember=str_replace("{rows}",$rows,$cmember);
 	$cmember=str_replace("{action}",$action,$cmember);
 	$cmember=str_replace("{sitename}",$sitename,$cmember);
-	$content=$cmember;
-	FFileRead("templates/admin.main.".$_SESSION['menu'].".html",$main);
-	FFileRead("../config/version", $version);
-	$query="select id from messages where member_id=1 and read_flag=0";
-	$q->query($query);
-	$q->next_record();
-	$main=str_replace("{version}", $version, $main);
-	$main=str_replace("{newmessages}",$q->nf(),$main);
-	$main=str_replace("{content}",$content,$main);
-	$main=str_replace("{status}",$sta,$main);
-	$main=str_replace("{title}",$title,$main);
-	$main=str_replace("{sitename}",$sitename,$main);
-	$main=str_replace("{webmasteremail}",$webmasteremail,$main);
-	$main=str_replace("{year}", date("Y"),$main);
-	$main=str_replace("{status}",$status,$main);
-	$main=str_replace("{search}",$search,$main);
-	$main=str_replace("{search2}",$search2,$main);
-	if($time_trigger==1)
-		$main=str_replace("{time_interval}",$time_interval,$main);
-	else 
-		$main=str_replace("{time_interval}","",$main);
-	if($month_start!='') $_SESSION['month_start']=$month_start;
-	if($month_end!='') $_SESSION['month_end']=$month_end;
-	if($day_start!='') $_SESSION['day_start']=$day_start;
-	if($day_end!='') $_SESSION['day_end']=$day_end;
-	if($year_start!='') $_SESSION['year_start']=$year_start;
-	if($year_end!='') $_SESSION['year_end']=$year_end;
-	echo $main.$script;
+	
+  $main = $cmember;
+
+  $main=str_replace("{status}",$status,$main);
+  $main=str_replace("{search}",$search,$main);
+  $main=str_replace("{search2}",$search2,$main);
+    
+  if($time_trigger==1)
+    $main=str_replace("{time_interval}",$time_interval,$main);
+  else 
+    $main=str_replace("{time_interval}","",$main);
+
+  if($month_start!='') $_SESSION['month_start']=$month_start;
+  if($month_end!='') $_SESSION['month_end']=$month_end;
+  if($day_start!='') $_SESSION['day_start']=$day_start;
+  if($day_end!='') $_SESSION['day_end']=$day_end;
+  if($year_start!='') $_SESSION['year_start']=$year_start;
+  if($year_end!='') $_SESSION['year_end']=$year_end;
+
+  
+  
+//	FFileRead("templates/admin.main.".$_SESSION['menu'].".html",$main);
+//	FFileRead("../config/version", $version);
+//	$query="select id from messages where member_id=1 and read_flag=0";
+//	$q->query($query);
+//	$q->next_record();
+//	$main=str_replace("{version}", $version, $main);
+//	$main=str_replace("{newmessages}",$q->nf(),$main);
+//	$main=str_replace("{content}",$content,$main);
+//	$main=str_replace("{status}",$sta,$main);
+//	$main=str_replace("{title}",$title,$main);
+//	$main=str_replace("{sitename}",$sitename,$main);
+//	$main=str_replace("{webmasteremail}",$webmasteremail,$main);
+//	$main=str_replace("{year}", date("Y"),$main);
+
+ // @todo: refactor aff.tr.php to use templating system	
+//  if (isset($_GET['menu'])) {
+//    $_SESSION['menu'] = $_GET['menu'];
+//  }
+  
+  print mwg_admin_decorate($main, '', '', $script);
+  
+//  genstall_admin_end($t, $main.$script, $notemplate);  
 ?>
