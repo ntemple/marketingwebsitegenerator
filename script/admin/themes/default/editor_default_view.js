0df205b3630f685918e4361fe4f5a985911c7a83
Nick Temple <ntemple@users.noreply.github.com>
2010-05-28 09:49:07 +0000

    updates and minor fixes

--- script/admin/themes/default/editor_default_view.js
+++ script/admin/themes/default/editor_default_view.js
@@ -1,7 +1,7 @@
 <script type="text/JavaScript">
 <!--
 function MM_jumpMenu(targ,selObj,restore){ //v3.0
-  eval(targ+".location='servlet.php?m=editor&menu=design&filename="+selObj.options[selObj.selectedIndex].value+"'");
+  eval(targ+".location='servlet.php?m=editor&menu=site&filename="+selObj.options[selObj.selectedIndex].value+"'");
   if (restore) selObj.selectedIndex=0;
 }
 //-->
