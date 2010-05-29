b7e8d6e194b6d92c5d83e40ef7843fd6e4a1e107
Nick Temple <ntemple@users.noreply.github.com>
2010-05-29 12:37:54 +0000

    no more butterflys

--- script/install/install-frames.php
+++ script/install/install-frames.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Buttefly Script Installation Wizard</title>
+<title>Marketing Website Generator (MWG) Installation Wizard</title>
 </head>
 <frameset rows="152,*" cols="*" framespacing="0" frameborder="no" border="0">
   <frame src="step2.php" name="topFrame" scrolling="No" noresize="noresize" id="topFrame" title="topFrame" />
