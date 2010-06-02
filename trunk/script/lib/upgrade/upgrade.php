062b1ec1ec8c730b8ac2f0ae5f084324568a8a39
commercemeister <commercemeister@c7ded231-71c2-9580-50d5-ff70528b3006>
2010-06-02 17:06:36 +0000

    modified update script

--- trunk/script/lib/upgrade/upgrade.php
+++ trunk/script/lib/upgrade/upgrade.php
@@ -18,11 +18,13 @@
 * Needs to be very fast as it will be run on every call
 */
 
+define('MWG_DB_VERSION', '1.4');
+
 $db = MWG::getDb();
 ob_start();
 upgrade($db);
 $debug = ob_get_clean();
-// print "<pre>\n$debug\n"; print_r($db); exit();
+print "<pre>\n$debug\n"; print_r($db); exit();
 
 
 function upgrade(mysqldb $db) {
@@ -35,38 +37,31 @@ function upgrade(mysqldb $db) {
     $db_version = '1.0';
   }
 
-  if ($db_version < 1.3) {
-    $lock = $db->get_value('select value from settings where name=?', 'lock');
-    if ($lock > 1) return $db_version;
+  if ($db_version >= MWG_DB_VERSION) return;
+ 
+  # perform upgrade
+  include('upgrade13.php');
 
-    if ($lock == 1) {
-      $db->query('update settings set value=2 where name=?', 'lock');
-    } else {
-      $db->query("insert into settings (box_type, name, value) values ('hidden', 'lock', 2)");
-    }
+ 
+  $lock = $db->get_value('select value from settings where name=?', 'lock');
+  if ($lock > 1) return $db_version;
 
-    include('upgrade13.php');
-    upgrade13($db, $db_version);
-
-    $db->query('update settings set value=1 where name=?', 'lock');
+  if ($lock == 1) {
+    $db->query('update settings set value=2 where name=?', 'lock');
+  } else {
+     $db->query("insert into settings (box_type, name, value) values ('hidden', 'lock', 2)");
   }
 
-  return $db_version;
+  upgrade_tables($db);
+  run_upgrade($db, 'settings.txt', 'on duplicate key update id=id');
+  run_upgrade($db, 'upgrade.txt'); 
 
-}
-
-function run_upgrade($db, $file) {
-  $txt = file_get_contents($file);
-  $queries = explode(";\n", $txt);
-  foreach ($queries as $q) {
-    $q = trim($q);
-    if ($q) {
-      print ".$q.\n";
-      $db->query($q);
-      print $db->_error;
-    }
-  }
+  $db->query("UPDATE mwg_setting SET value=? WHERE name = 'site_dbversion'", MWG_DB_VERSION);
+  $db->query('update settings set value=1 where name=?', 'lock');
 
   return $db->get_value('select value from mwg_setting where name=?', 'site_dbversion');
+
 }
+
+
 
