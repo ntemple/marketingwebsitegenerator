3f2f79fe0f4878a0612448bb01079a3341b39d0e
Nick Temple <ntemple@users.noreply.github.com>
2010-06-02 17:06:36 +0000

    modified update script

--- script/lib/upgrade/upgrade13.php
+++ script/lib/upgrade/upgrade13.php
@@ -18,52 +18,61 @@
 * Needs to be very fast as it will be run on every call
 */
 
-function upgrade13($db, $db_version) {
+function upgrade_upgrade_tables($db) {
 
   # Upgrade members table
-  $cols = $db->get_results('show columns from members');
+  $table = upgrade_check_fields('members');
+  if (isset($table['stormpay_email'])) $db->query('alter table members drop stormpay_email');
+  if (isset($table['p_stormpay_email']))  $db->query('alter table members drop p_stormpay_email'); 
+  if (!isset($table['admin'])) $db->query("ALTER TABLE `members` ADD `admin` CHAR( 1 ) NOT NULL DEFAULT '0' AFTER `jv`");
 
-  $admin = false;
-
-  # Members Table
-  foreach ($cols as $field) {
-    switch ($field['Field']) {
-      case 'stormpay_email':    $db->query('alter table members drop stormpay_email'); break;
-      case 'p_stormpay_email':  $db->query('alter table members drop p_stormpay_email'); break;
-      case 'admin': $admin = true;
-    }
-  }
-  if (! $admin) $db->query("ALTER TABLE `members` ADD `admin` CHAR( 1 ) NOT NULL DEFAULT '0' AFTER `jv`");
-
-
-  $tables = $db->get_results("show tables like 'mwg_setting'");
-  if (count($tables) == 0) {
+  $table = upgrade_check_fields('mwg_setting');
+  if (! $table) {
      run_upgrade($db, MWG_BASE . '/lib/upgrade/table.setting.txt');
   }
 
- $tables = $db->get_results("show tables like 'mwg_gizmo'");
-  if (count($tables) == 0) {
+  # this may produce an error that can safely be ignored
+  $db->query("ALTER TABLE `mwg_setting` ADD UNIQUE ( `name`)");
+
+  $table = upgrade_check_fields('mwg_gizmo');
+  if (! $table) {
      run_upgrade($db, MWG_BASE . '/lib/upgrade/table.gizmo.txt');
+     $table = upgrade_check_fields('mwg_gizmo');
+  }
+  # We now have to upgrade the gizmo table
+  if (!isset($table['position'])) $db->query("ALTER TABLE `mwg_gizmo` ADD `position` VARCHAR( 255 ) 
+                                              NOT NULL AFTER `title` , ADD `ordre` INT NOT NULL AFTER `position`");
+  $db->query("UPDATE mwg_setting SET value='1.3' WHERE name = 'site_dbversion'");
+}
+
+function upgrade_check_fields($table) {
+  $tables = $db->get_results("show tables like ?", $table);
+  if (count($tables) == 0) return false;
+
+  $fields = array();
+
+  $cols = $db->get_results("show columns from $table");
+  foreach($cols as $field) {
+    $fields[$field['Field']] = $field;
+  }
+  return $fields; 
+}
+
+
+function run_upgrade($db, $file, $extra = '') {
+  $txt = file_get_contents($file);
+  $queries = explode(";\n", $txt);
+  foreach ($queries as $q) {
+    $q = trim($q);
+    if (! $q) continue;
+    $q .= ' ' . $extra;
+    print ".$q.\n";
+  #  $db->query($q);
+
+    print $db->_error;
   }
 
-  $db_version = $db->get_value('select value from mwg_setting where name=?', 'site_dbversion');
-  if (! $db_version) run_upgrade($db, MWG_BASE . '/lib/upgrade/table.settings.data.txt');
-  $db_version = $db->get_value('select value from mwg_setting where name=?', 'site_dbversion');
-
-
-  $db_version = run_upgrade($db, MWG_BASE . '/lib/upgrade/upgrade-1.1.txt');
-
-  # Add the subtitle
-  if (! $db->get_value('select id from mwg_setting where name=?', 'theme_subtitle')) 
-  $db->query("INSERT INTO `mwg_setting` (`id` ,`category` ,`group` ,`name` ,`value` ,`default_value` ,`input_type` ,`label` ,`help_text` ,`options` ,`rank`)VALUES (NULL , 'Global Site Settings', NULL , 'theme_subtitle', 'Just Another Marketing Website Generator Site', '', 'input', 'Site Subtitle', 'Some themes (wordpress) have the option to use a subtitle. Enter that here.', '', '0')");
-
-
-  $db_version = run_upgrade($db, MWG_BASE . '/lib/upgrade/upgrade-1.2.txt');
-
-  $db->query("UPDATE mwg_setting SET value='1.3' WHERE name = 'site_dbversion'");
-  $db_version = $db->get_value('select value from mwg_setting where name=?', 'site_dbversion'); 
-
-  return $db_version;
+  return $db->get_value('select value from mwg_setting where name=?', 'site_dbversion');
 }
 
 
